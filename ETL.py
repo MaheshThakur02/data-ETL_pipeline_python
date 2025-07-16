0fba08bb8ffa88c3596db0a8f6b441c9e2ce9f76
MaheshThakur02 <mp6590648@gmail.com>
2025-07-17 05:12:30 +0530

    Day 1: Data ETL Pipeline

---
+++ ETL.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
+
+
+# Step 1: Load Data
+df = pd.read_csv("car_done.csv")
+print("Before dropna:", df.shape)
+
+# Step 2: Clean Data
+df.dropna(inplace=True)
+print("Before dropna:", df.shape)
+
+
+# Step 3: Connect to PostgreSQL
+engine = create_engine('postgresql://postgres:Mahesh1234@localhost:5432/testdb')
+
+# Step 4: Load to PostgreSQL
+df.to_sql("cars", engine, if_exists="replace", index=False)
+print("Data loaded successfully!")
